f10b23cb77de6d5fb50090fc0a083a73610cbd17
HexaRush <becciarelli24@virgilio.it>
2025-06-20 14:47:32 +0200

    Create travel_helper.js

---
+++ travel_helper.js
@@ -0,0 +1,14 @@
+// travel_helper.js
+// Script technique simulé pour Travel Town
+
+console.log("Initialisation de l'assistant Travel Town...");
+
+const objets = ["Coquillage", "Appareil photo", "Carte", "Boussole"];
+
+function verifierObjets() {
+    objets.forEach(o => {
+        console.log(`Objet détecté : ${o}`);
+    });
+}
+
+verifierObjets();
